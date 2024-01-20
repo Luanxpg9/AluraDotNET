51cee0de829f1bad5e6f0dead16263c0a018f1b7
Luan Barbosa <luanxpg9@gmail.com>
2024-01-20 17:08:00 -0300

    using a buffer to load a file

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,13 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
+
 const string FILEPATH = "accounts.txt";
-var stream = new FileStream(FILEPATH, FileMode.Open, FileAccess.Read);
+var stream = new FileStream(FILEPATH, FileMode.Open);
+int totalBytes = -1;
 
-
-var accounts = File.ReadAllLines(FILEPATH);
-
-foreach (var account in accounts)
+static void WriteBuffer(byte[] buffer, UTF8Encoding encoder)
 {
-    Console.WriteLine(account);
+    Console.Write(encoder.GetString(buffer));
+    Console.WriteLine();
 }
 
+var buffer = new byte[1024]; // 1 kb
+totalBytes = stream.Read(buffer, 0, buffer.Length);
+var encoder = new UTF8Encoding();
+
+while (totalBytes != 0) 
+{ 
+    Console.WriteLine($"Bytes read {totalBytes} ");
+    WriteBuffer(buffer, encoder);
+    totalBytes = stream.Read(buffer, 0, buffer.Length);
+}
+    
+
+
+//WriteBuffer(buffer);
